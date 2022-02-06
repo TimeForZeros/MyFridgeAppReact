861ee6fb48f915b067a0895f294be14f785cb037
Victor Gonzalez <46080484+TimeForZeros@users.noreply.github.com>
2022-02-05 20:14:06 -0800

    commonjs -> module

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // server/index.js
-const path = require('path');
-const express = require('express');
+import {resolve, dirname} from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(resolve(__dirname, './client/build')));
 
 // // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -17,7 +20,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  res.sendFile(resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.get("/api", (req, res) => {
